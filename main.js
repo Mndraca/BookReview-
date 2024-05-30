899060c3f8aba771b31e42a017f577b0c31fedff
Milutin25 <stefanovicfamily@Stefanovics-iMac.local>
2024-05-30 13:43:12 +0200

    Favorites updated and delete button

--- main.js
+++ main.js
@@ -121,20 +121,33 @@ let books = [
 function booksDisplayed() {
   const bookContainer = document.getElementById("bookSection");
   document.getElementById("favorites").classList.add("hidden");
+  bookContainer.innerHTML = ''; 
   books.forEach((book) => {
     const bookList = document.createElement("ul");
     bookList.classList.add("bookList");
-    bookList.innerHTML += `<li><img src="${book.image}"></li>
+    bookList.setAttribute("id", `book-${book.id}`);
+    bookList.innerHTML += `
+      <li><button onclick="removeBook(${book.id})" class="remove-button" style="background-color: #f4f4f4; color:#333; border:none " ><i class="far fa-times"></i></button></li>
+      <li><img src="${book.image}"></li>
       <li class="book-title">${book.title}</li>
       <li class="book-author">${book.author}</li>
       <li><button onclick="addToFavorites(${book.id})">Add to Favorites <i class="fa-regular fa-heart"></i></button></li>
-      `;
+    `;
 
     bookContainer.appendChild(bookList);
   });
 }
 
 document.addEventListener("DOMContentLoaded", booksDisplayed);
+
+function removeBook(bookId) {
+  books = books.filter((book) => book.id === bookId);
+
+  const bookElement = document.getElementById(`book-${bookId}`);
+  if (bookElement) {
+    bookElement.remove();
+  }
+}
 
 // Function for a search button - books from the Home page will be hidden after user clicks on the search button, only results will be displayed
 const searchButton = document.getElementById("searchButton");
@@ -143,10 +156,7 @@ searchButton.addEventListener("click", (event) => {
   event.preventDefault();
   document.getElementById("bookSection").classList.add("hidden");
   const bookResults = document.getElementById("bookResults");
-  const searchInput = document
-    .getElementById("searchInput")
-    .value.toLowerCase()
-    .trim();
+  const searchInput = document.getElementById("searchInput").value.toLowerCase().trim();
 
   let filteredBooks = books.filter((book) => {
     return (
@@ -156,19 +166,17 @@ searchButton.addEventListener("click", (event) => {
   });
 
   if (!searchInput) {
-    bookResults.textContent =
-      "Please enter a book or author that you want to review.";
+    bookResults.textContent = "Please enter a book or author that you want to review.";
     return;
   }
 
   bookResults.innerHTML = "";
   if (filteredBooks.length > 0) {
-    bookResults.innerHTML = "";
     filteredBooks.forEach((book) => {
       const foundBooksImage = document.createElement("div");
       foundBooksImage.classList.add("foundBooksImage");
-      foundBooksImage.innerHTML += `<img src="${book.image}">
-        `;
+      foundBooksImage.innerHTML += `<img src="${book.image}">`;
+      
       const foundBooksText = document.createElement("div");
       foundBooksText.classList.add("foundBooksText");
       foundBooksText.innerHTML += `
@@ -228,27 +236,48 @@ const favoriteBooksIds = new Set();
 
 function addToFavorites(bookId) {
   document.getElementById("favorites").classList.remove("hidden");
-  document.getElementById("bookSection").classList.add("hidden");
+
   const book = books.find((book) => book.id === bookId);
   if (!book || favoriteBooksIds.has(bookId)) return;
 
   const favoriteBooksList = document.getElementById("favorites");
   const clonedBooks = document.createElement("li");
 
-  clonedBooks.className = "cloned";
+  clonedBooks.className = "bookList";
   clonedBooks.innerHTML = `
     <li><img src="${book.image}"></li>
     <li>${book.title}</li>
     <li>${book.author}</li>
-    <li><i>Review: ${book.review}</i></li>
-    <li><button id="removeFromFavorites" onclick="removeFromFavorites${bookId}">Remove from favorites</button></li>
-    `;
+    ${book.review ? `<li><i>Review: ${book.review}</i></li>` : ''}
+    <li><button id="removeFromFavorites" onclick="removeFromFavorites(${bookId})">Remove from favorites</button></li>
+  `;
 
   favoriteBooksList.appendChild(clonedBooks);
 
   favoriteBooksIds.add(bookId);
 }
 
-//function removeFromFavorites (bookId) {
-//const book = books.find(book => book.id === bookId);
-//}
+function removeFromFavorites(bookId) {
+  favoriteBooksIds.delete(bookId);
+
+  const favoriteBookElement = document.getElementById(`favoriteBook-${bookId}`);
+  if (favoriteBookElement) {
+    favoriteBookElement.remove();
+  }
+
+  if (favoriteBooksIds.size === 0) {
+    document.getElementById("favorites").classList.add("hidden");
+  }
+}
+
+
+const favoritesLink = document.getElementById("favoritesLink"); 
+  favoritesLink.addEventListener('click', () => {
+  document.getElementById("favorites").classList.remove("hidden"); 
+} )
+
+const homePage = document.getElementById("homePage"); 
+homePage.addEventListener('click', () => {
+  document.getElementById("bookSection").classList.remove("hidden");
+  document.getElementById("bookResults").classList.add("hidden");
+} )
